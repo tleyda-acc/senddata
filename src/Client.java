9ad371c91a8aecef34224635f6a5e5510837cf1f
Troy Leyda <troy.leyda@accenture.com>
2020-12-05 23:01:11 -0600

    updates

--- src/Client.java
+++ src/Client.java
@@ -1,25 +1,32 @@
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
 public class Client {
     private Socket socket  = null;
-    ObjectOutputStream output = null;
+    private ObjectOutputStream output = null;
+    private ObjectInputStream input = null;
 
     public void runClient() {
         try {
             socket = new Socket("127.0.0.1",  5555);
             output = new ObjectOutputStream(socket.getOutputStream());
+            input = new ObjectInputStream(socket.getInputStream());
 
             IntMessage intMessage = new IntMessage(22);
             output.writeObject(intMessage);
             output.flush();
 
+            getMessage();
+
             StringMessage strMessage = new StringMessage("Check it out!");
             output.writeObject(strMessage);
             output.flush();
+
+            getMessage();
 
             Message done = new Message("done");
             output.writeObject(done);
@@ -31,6 +38,20 @@ public class Client {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void getMessage() throws IOException, ClassNotFoundException {
+        Message message = (Message)input.readObject();
+
+        if (message.getType().equals("string")) {
+            StringMessage stringMessage = (StringMessage) message;
+            System.out.println("String message: " + stringMessage.getValue());
+        } else if (message.getType().equals("integer")) {
+            IntMessage intMessage = (IntMessage) message;
+            System.out.println("Int message: " + intMessage.getValue());
         }
     }
 
--- src/Server.java
+++ src/Server.java
@@ -1,5 +1,6 @@
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -7,6 +8,7 @@ public class Server {
     private Socket socket = null;
     private ServerSocket server = null;
     private ObjectInputStream input = null;
+    private ObjectOutputStream output = null;
 
     public void runServer() {
         try {
@@ -14,6 +16,7 @@ public class Server {
             System.out.println("Waiting for client...");
             socket = server.accept();
             input = new ObjectInputStream(socket.getInputStream());
+            output = new ObjectOutputStream(socket.getOutputStream());
 
             boolean done = false;
 
@@ -26,9 +29,15 @@ public class Server {
                     if (message.getType().equals("string")) {
                         StringMessage stringMessage = (StringMessage) message;
                         System.out.println("String message: " + stringMessage.getValue());
+                        IntMessage intMessage = new IntMessage(stringMessage.getValue().length());
+                        output.writeObject(intMessage);
+                        output.flush();
                     } else if (message.getType().equals("integer")) {
                         IntMessage intMessage = (IntMessage) message;
                         System.out.println("Int message: " + intMessage.getValue());
+                        StringMessage strMessage = new StringMessage("You sent the number: " + intMessage.getValue());
+                        output.writeObject(strMessage);
+                        output.flush();
                     }
                 }
             }
